2fe69eae541cd6a83326c6c52cc67dbc873b3487
Megan <megan.nyakina@student.moringaschool.com>
2024-07-02 16:05:20 +0300

    adds array filter method

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching( drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+
+function fuzzyMatch(drivers, letter) {
+    letter = letter.toLowerCase();
+    return drivers.filter( driver => driver.toLowerCase().startsWith(letter));
+}
+
+
+function matchName (drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+
+
+
+
+
+
